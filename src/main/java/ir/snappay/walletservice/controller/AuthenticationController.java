141212887bb59099cd43aa8415de5df18bfebd01
f.farahmand <farhad.farahmand@kishsys.com>
2024-10-14 07:08:05 +0330

    add logback simple config

--- src/main/java/ir/snappay/walletservice/controller/AuthenticationController.java
+++ src/main/java/ir/snappay/walletservice/controller/AuthenticationController.java
@@ -8,6 +8,8 @@ import ir.snappay.walletservice.dto.RegisterUserDto;
 import ir.snappay.walletservice.dto.UserDto;
 import ir.snappay.walletservice.service.AuthenticationService;
 import jakarta.validation.Valid;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 @RestController
 @Tag(name= "authentication api", description= "user this api to signup and login users.")
+@Log
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
@@ -27,6 +30,8 @@ public class AuthenticationController {
     @PostMapping("v1/signup")
     @Operation(summary="register a new user to wallet service")
     public UserDto register(@Valid @RequestBody RegisterUserDto dto){
+        log.info("start register api");
+
         return authenticationService.signup(dto);
     }
 
